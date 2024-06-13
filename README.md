01a1b7a709d4dd52d34e227e31682c268c3338fd
ostermine <enemrulit@gmail.com>
2024-06-13 14:30:21 +0300

    minor updates

--- README.md
+++ README.md
@@ -5,7 +5,22 @@
 - If u have docker with docker-compose: `docker compose up -d`
 - Or: `pip install -r requirements.txt` `python main.py`
 ----
-- Example of using: `YOURSITE.COM/?local_address=1.2.3.4&dns=8.8.8.8,8.8.4.4&endpoint=228.14.88.0&server_pubkey=ADmkdlsnghklsnklsdnlkh=&allowed_ips=192.168.0.0/24,127.0.0.1`
+- Example of using: `curl -X GET "YOURSITE.COM/?local_address=1.2.3.4&dns=8.8.8.8,8.8.4.4&endpoint=228.14.88.0&server_pubkey=ADmkdlsnghklsnklsdnlkh=&allowed_ips=192.168.0.0/24,127.0.0.1&interface=wg-clients" > vpn123.conf`
+
+- Example out:
+```
+# /interface wireguard peer add public-key="d70LEN7123wBjdA0F5xhk=" interface=wg-clients allowed-address=172.16.1.25/32 persistent-keepalive=1m
+[Interface]
+PrivateKey = HONY3ahbMNW253a0fduSrYdubc=
+ListenPort = 51820
+Address = 172.16.1.25/32
+DNS = 172.16.1.1,8.8.8.8
+
+[Peer]
+PublicKey = GxiJvk8TJKD112DoZFdJ6mUzzW0=
+AllowedIPs = 0.0.0.0/0
+Endpoint = 1.2.3.4:51800
+```
 ----
 ## WHY?
 - I needs to fast generate good config between client and Mikrotik (mikrotik's cant normal to generate clients configs)
--- main.py
+++ main.py
@@ -14,15 +14,14 @@ def generate_keypair() -> tuple[str]:
     return (private_key_str, public_key_str)
 
 @app.route('/')
-def hello_world():
+def generate_wg_config():
     keypair = generate_keypair()
-    # dnses = ""
     local_address = request.args.get(key = "local_address", default = "")
     dns = request.args.get(key = "dns", default = "")
-    # dns2 = request.args.get(key = "dns2", default = "")
     endpoint = request.args.get(key = "endpoint", default = "")
     server_pubkey = request.args.get(key = "server_pubkey", default = "")
     allowed_ips = request.args.get(key = "allowed_ips", default = "")
+    interface = request.args.get(key = "interface", default = "wg0")
 
     if not all([
         local_address,
@@ -31,17 +30,11 @@ def hello_world():
         server_pubkey,
         allowed_ips
     ]):
-        return "not enough args ._.<br><br>example GET request with args: <b>/?local_address=1.2.3.4&dns=8.8.8.8,8.8.4.4&endpoint=228.14.88.0&server_pubkey=ADmkdlsnghklsnklsdnlkh=&allowed_ips=192.168.0.0/24,127.0.0.1</b>"
-    
-    # if all([dns1, dns2]):
-    #     dnses = f"{dns1}, {dns2}"
-    # else:
-    #     dnses = dns1
+        return "not enough args ._.\nexample GET request with args: \n/?local_address=1.2.3.4&dns=8.8.8.8,8.8.4.4&endpoint=228.14.88.0&server_pubkey=ADmkdlsnghklsnklsdnlkh=&allowed_ips=192.168.0.0/24,127.0.0.1"
 
     return \
-        f"""
+        f"""# /interface wireguard peer add public-key="{keypair[1]}" interface={interface} allowed-address={local_address} persistent-keepalive=1m
 [Interface]
-# public key = {keypair[1]}
 PrivateKey = {keypair[0]}
 ListenPort = 51820
 Address = {local_address}
@@ -51,9 +44,8 @@ DNS = {dns}
 PublicKey = {server_pubkey}
 AllowedIPs = {allowed_ips}
 Endpoint = {endpoint}
-        """.replace("\n", "<br>")
+        """
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8055)
-    # print(generate_keypair())
